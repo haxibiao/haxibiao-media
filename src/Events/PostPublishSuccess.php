3fb2514252688ead4dbaf154a52b2474c7343d25
xxm <xiaoxinming@haxibiao.com>
2020-09-17 00:38:27 +0800

    DZ-1230 feat:添加动态发布成功事件

---
+++ src/Events/PostPublishSuccess.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Haxibiao\Media\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class PostPublishSuccess
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $post;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct($post)
+    {
+        $this->post = $post;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new PrivateChannel('channel-name');
+    }
+}
