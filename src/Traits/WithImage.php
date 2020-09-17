28b150dc5d85d170e1032937906922f5aa14b360
吴秀璋 <wuxiuzhang@haxibiao.com>
2020-09-17 21:53:06 +0800

    fixed：懂外贸反馈失败

--- src/Traits/WithImage.php
+++ src/Traits/WithImage.php
@@ -15,7 +15,7 @@ trait WithImage
         if (in_array(config('app.name'), [
             'yinxiangshipin', 'dongmiaomu', 'caohan', 'dongmeiwei',
             'dongdianyi', 'quanminwenti', 'dongdaima', 'ainicheng',
-            'dongshouji', 'dongyundong', 'dongwaiyu'
+            'dongshouji', 'dongyundong', 'dongwaiyu', 'dongwaimao'
         ])) {
             return $this->morphToMany(Image::class, 'imageable', 'imageable')
                 ->withTimestamps();
