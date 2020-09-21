67088ca09facfc2679b6634b57ee35aced8887cc
吴秀璋 <wuxiuzhang@haxibiao.com>
2020-09-21 15:17:06 +0800

    fixed:不约她iamges找不

--- src/Traits/WithImage.php
+++ src/Traits/WithImage.php
@@ -15,7 +15,7 @@ trait WithImage
         if (in_array(config('app.name'), [
             'yinxiangshipin', 'dongmiaomu', 'caohan', 'dongmeiwei',
             'dongdianyi', 'quanminwenti', 'dongdaima', 'ainicheng',
-            'dongshouji', 'dongyundong', 'dongwaiyu', 'dongwaimao'
+            'dongshouji', 'dongyundong', 'dongwaiyu', 'dongwaimao', 'buyueta'
         ])) {
             return $this->morphToMany(Image::class, 'imageable', 'imageable')
                 ->withTimestamps();
