3c05e59c40eabce8b5afd248a54661a1f0949380
唐淑娟 <tangshujuan@haxifang.cn>
2020-11-11 09:16:32 +0800

    modify get app_name

--- src/Traits/SpiderRepo.php
+++ src/Traits/SpiderRepo.php
@@ -18,7 +18,7 @@ trait SpiderRepo
     {
         // 通过config来控制接口开关 && 动态配置控制每用户日最大解析数
         throw_if(config('media.spider.enable') === false, UserException::class, '解析失败,功能维护中,请稍后再试!');
-        if (!in_array(env('APP_NAME'), [
+        if (!in_array(config('app.name'), [
             'yinxiangshipin', 'ainicheng', 'ablm',
             'youjianqi', 'nashipin', 'dongdianhai',
             'jinlinle', 'damei', 'haxibiao','dongwaimao'
