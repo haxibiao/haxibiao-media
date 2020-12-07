90e6ec6950f641128eaeed775c8007ed07e1e6f2
曾达威 <zengdawei@haxibiao.com>
2020-12-07 19:00:36 +0800

    update: 更新movies表结构和movie:sync脚本

---
+++ database/migrations/2020_12_07_185757_add_columns_on_movies_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddColumnsOnMoviesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('movies', function (Blueprint $table) {
+            if (!Schema::hasColumn('movies', 'rank')) {
+                $table->tinyInteger('rank')->default(0)->comment('权重');
+                $table->string('country')->nullable()->comment('国家');
+                $table->string('subname')->nullable()->comment('别名');
+                $table->string('tags')->nullable()->comment('标签');
+                $table->string('lang')->nullable()->comment('语言');
+                $table->unsignedInteger('hits')->nullable()->comment('点击次数');
+                $table->unsignedInteger('type_name')->nullable()->comment('类型名');
+                $table->double('score')->nullable()->comment('评分');
+            }
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
--- src/Console/MovieSync.php
+++ src/Console/MovieSync.php
@@ -40,73 +40,81 @@ class MovieSync extends Command
      */
     public function handle()
     {
-        if (!Schema::hasTable('movies')){
+        if (!Schema::hasTable('movies')) {
             return $this->error("没有movies表");
         }
 
-        $region     = $this->option('region');
-        $type       = $this->option('type');
-        $style      = $this->option('style');
-        $year       = $this->option('year');
-        $producer   = $this->option('producer');
-        $actors     = $this->option('actors');
+        $region   = $this->option('region');
+        $type     = $this->option('type');
+        $style    = $this->option('style');
+        $year     = $this->option('year');
+        $producer = $this->option('producer');
+        $actors   = $this->option('actors');
 
         $success = 0;
         $fail    = 0;
         $total   = 0;
 
         DB::connection('package_media_chain')->table('movies')
-            ->when( $region , function ($q) use ($region){
-                $q->where('region',$region);
-            })->when( $type , function ($q) use ($type){
-                $q->where('type',$type);
-            })->when( $style , function ($q) use ($style){
-                $q->where('style',$style);
-            })->when( $year , function ($q) use ($year){
-                $q->where('year',$year);
-            })->when( $producer , function ($q) use ($producer){
-                $q->where('producer',$producer);
-            })->when( $actors , function ($q) use ($actors){
-                $actors = explode(',',$actors);
-                $q->where(function ($q)use($actors){
-                    foreach ($actors as $actor){
-                        if(!trim($actor)){
-                            continue;
-                        }
-                        $q = $q->orWhere('actors','like','%'.$actor.'%');
+            ->when($region, function ($q) use ($region) {
+                $q->where('region', $region);
+            })->when($type, function ($q) use ($type) {
+            $q->where('type', $type);
+        })->when($style, function ($q) use ($style) {
+            $q->where('style', $style);
+        })->when($year, function ($q) use ($year) {
+            $q->where('year', $year);
+        })->when($producer, function ($q) use ($producer) {
+            $q->where('producer', $producer);
+        })->when($actors, function ($q) use ($actors) {
+            $actors = explode(',', $actors);
+            $q->where(function ($q) use ($actors) {
+                foreach ($actors as $actor) {
+                    if (!trim($actor)) {
+                        continue;
                     }
-                });
-            })->orderBy('id')->chunk(100,function ($movies)use( &$fail,&$success,&$total){
-                foreach ($movies as $movie){
-                    $total++;
-                    $movie = @json_decode(json_encode($movie), true);
-                    DB::beginTransaction();
-                    try {
-                        $model = Movie::firstOrNew([
-                            'name'=>data_get($movie,'name')
-                        ]);
-                        // 不同名的movie，直接导入
-                        $model->forceFill(array_only($movie,[
-                            'introduction',
-                            'cover',
-                            'producer',
-                            'year',
-                            'region',
-                            'actors',
-                            'miner',
-                            'count_series',
-                            'data',
-                        ]))->save();
-                        DB::commit();
-                        $success++;
-                        $this->info('已成功导入：'.$success.'部,当前导入:'.data_get($movie,'name'),'成功');
-                    } catch (\Exception $ex) {
-                        DB::rollback();
-                        $fail++;
-                        $this->info('已成功导入：'.$success.'部,当前导入:'.data_get($movie,'name'),'失败');
-                    }
+                    $q = $q->orWhere('actors', 'like', '%' . $actor . '%');
                 }
             });
-            $this->info('共检索出'.$total.'部电影,成功导入：'.$success.'部,失败：'.$fail.'部');
+        })->orderBy('id')->chunk(100, function ($movies) use (&$fail, &$success, &$total) {
+            foreach ($movies as $movie) {
+                $total++;
+                $movie = @json_decode(json_encode($movie), true);
+                DB::beginTransaction();
+                try {
+                    $model = Movie::firstOrNew([
+                        'name' => data_get($movie, 'name'),
+                    ]);
+                    // 不同名的movie，直接导入
+                    $model->forceFill(array_only($movie, [
+                        'introduction',
+                        'cover',
+                        'producer',
+                        'year',
+                        'region',
+                        'actors',
+                        'miner',
+                        'count_series',
+                        'rank',
+                        'country',
+                        'subname',
+                        'score',
+                        'tags',
+                        'hits',
+                        'lang',
+                        'type_name',
+                        'data',
+                    ]))->save();
+                    DB::commit();
+                    $success++;
+                    $this->info('已成功导入：' . $success . '部,当前导入:' . data_get($movie, 'name'), '成功');
+                } catch (\Exception $ex) {
+                    DB::rollback();
+                    $fail++;
+                    $this->info('已成功导入：' . $success . '部,当前导入:' . data_get($movie, 'name'), '失败');
+                }
+            }
+        });
+        $this->info('共检索出' . $total . '部电影,成功导入：' . $success . '部,失败：' . $fail . '部');
     }
 }
