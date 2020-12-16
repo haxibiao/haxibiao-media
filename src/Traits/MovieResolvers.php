ebbb409ffad3357e9aac334d8dbc328e2625e6e6
张博 <zhangbo@haxifang.cn>
2020-12-16 11:39:34 +0800

    影视详情分类

--- src/Traits/MovieResolvers.php
+++ src/Traits/MovieResolvers.php
@@ -8,7 +8,33 @@ trait MovieResolvers
 {
     public function resolversCategoryMovie($root, $args, $content, $info)
     {
-        return Movie::where('region', $args['category']);
+        $region     = data_get($args,'region');
+        $type   = data_get($args,'type');
+        $style   = data_get($args,'style');
+        $country   = data_get($args,'country');
+        $lang   = data_get($args,'lang');
+
+        $qb = Movie::orderByDesc("created_at");
+        if($region){
+            $qb = $qb->where('region', $region);
+        }
+        //按电影类型分类
+        if($type){
+            $qb = $qb->where('type', $type);
+        }
+        //按电影风格分类
+        if($style){
+            $qb = $qb->where('style', $style);
+        }
+        //按国家分类
+        if($country){
+            $qb = $qb->where('country', $country);
+        }
+        //按语言分类
+        if($lang){
+            $qb = $qb->where('lang', $lang);
+        }
+        return $qb;
     }
 
     public function resolversMovie($root, $args, $content, $info)
