c5a607787335af804c31cffe17fd128952a2bf12
唐淑娟 <tangshujuan@haxifang.cn>
2020-12-16 18:48:28 +0800

    YXSP-101 fix migration

---
+++ database/migrations/2020_12_03_102739_add_series_id_on_movie_histories_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddSeriesIdOnMovieHistoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('movie_histories', function (Blueprint $table) {
+            if (!Schema::hasColumn('movie_histories', 'series_id')) {
+                $table->unsignedInteger('series_id')->nullable()->default(1)->index()->comment('集数index,记录在数组中的偏移量');
+            }
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
