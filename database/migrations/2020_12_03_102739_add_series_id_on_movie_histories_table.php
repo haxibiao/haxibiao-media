33ea9cb0c658899e8b2b871d4a3bb914bce10e03
yangqian <yangqian@haxifang.cn>
2020-12-17 11:02:23 +0800

    调整media中的migrate

--- database/migrations/2020_12_03_102739_add_series_id_on_movie_histories_table.php
+++ database/migrations/2020_12_03_102739_add_series_id_on_movie_histories_table.php
@@ -13,11 +13,11 @@ class AddSeriesIdOnMovieHistoriesTable extends Migration
      */
     public function up()
     {
-        Schema::table('movie_histories', function (Blueprint $table) {
-            if (!Schema::hasColumn('movie_histories', 'series_id')) {
-                $table->unsignedInteger('series_id')->nullable()->default(1)->index()->comment('集数index,记录在数组中的偏移量');
-            }
-        });
+        // Schema::table('movie_histories', function (Blueprint $table) {
+        //     if (!Schema::hasColumn('movie_histories', 'series_id')) {
+        //         $table->unsignedInteger('series_id')->nullable()->default(1)->index()->comment('集数index,记录在数组中的偏移量');
+        //     }
+        // });
     }
 
     /**
