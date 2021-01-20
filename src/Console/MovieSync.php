606ccca41328d8230576607e3471ecbd294e61e7
ivan <ivan@haxibiao.com>
2021-01-20 17:18:13 +0800

    重构所有app nova models到breeze

--- src/Console/MovieSync.php
+++ src/Console/MovieSync.php
@@ -40,6 +40,14 @@ class MovieSync extends Command
      */
     public function handle()
     {
+        if (env('DB_PASSWORD_MEDIA') == null) {
+            $db_password_media = $this->ask("请注意 env('DB_PASSWORD_MEDIA') 未设置，正在用env('DB_PASSWORD'), 如果需要不同密码请输入或者[enter]跳过");
+            if ($db_password_media) {
+                config(['database.connections.mediachain.password' => $db_password_media]);
+                $this->confirm("已设置media的db密码，继续吗? ");
+            }
+        }
+
         if (!Schema::hasTable('movies')) {
             return $this->error("没有movies表");
         }
