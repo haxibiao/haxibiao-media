09c6f83923b7a2b0bd52d272093945fbd9816629
唐淑娟 <tangshujuan@haxifang.cn>
2021-01-26 16:25:39 +0800

    fix movie ut

--- tests/Feature/GraphQL/MovieTest.php
+++ tests/Feature/GraphQL/MovieTest.php
@@ -11,10 +11,11 @@ class MovieTest extends GraphQLTestCase
 
     /**
      * @group  movie
+     * @group  testMovieQuery
      */
     public function testMovieQuery()
     {
-        $query     = file_get_contents(__DIR__ . '/movie/movieQuery.gql');
+        $query = file_get_contents(__DIR__ . '/movie/movieQuery.gql');
         $variables = [
             'movie_id' => 1,
         ];
@@ -23,10 +24,11 @@ class MovieTest extends GraphQLTestCase
 
     /**
      * @group  movie
+     * @group  testRecommendMovieQuery
      */
     public function testRecommendMovieQuery()
     {
-        $query     = file_get_contents(__DIR__ . '/movie/recommendMovieQuery.gql');
+        $query = file_get_contents(__DIR__ . '/movie/recommendMovieQuery.gql');
         $variables = [
         ];
         $this->startGraphQL($query, $variables);
@@ -34,10 +36,11 @@ class MovieTest extends GraphQLTestCase
 
     /**
      * @group  movie
+     * @group  testPostWithMoviesQuery
      */
     public function testPostWithMoviesQuery()
     {
-        $query     = file_get_contents(__DIR__ . '/movie/postWithMoviesQuery.gql');
+        $query = file_get_contents(__DIR__ . '/movie/postWithMoviesQuery.gql');
         $variables = [
         ];
         $this->startGraphQL($query, $variables);
@@ -45,6 +48,7 @@ class MovieTest extends GraphQLTestCase
 
     /**
      * @group  movie
+     * @group  testCategoryMovieQuery
      */
     public function testCategoryMovieQuery()
     {
@@ -58,14 +62,14 @@ class MovieTest extends GraphQLTestCase
         //查询2012年的美剧
         $variables = [
             'region' => 'MEI',
-            'year'   => '2012',
+            'year' => '2012',
         ];
         $this->startGraphQL($query, $variables);
 
         //按热度排序2020年韩剧
         $variables = [
             'region' => 'HAN',
-            'year'   => '2020',
+            'year' => '2020',
             'scopes' => 'HOT',
         ];
         $this->startGraphQL($query, $variables);
@@ -74,11 +78,12 @@ class MovieTest extends GraphQLTestCase
 
     /**
      * @group  movie
+     * @group  testSearchMovieQuery
      */
     public function testSearchMovieQuery()
     {
-        $query     = file_get_contents(__DIR__ . '/movie/searchMoviesQuery.gql');
-        $keyword   = Movie::first()->name;
+        $query = file_get_contents(__DIR__ . '/movie/searchMoviesQuery.gql');
+        $keyword = Movie::first()->name;
         $variables = [
             'keyword' => $keyword,
         ];
@@ -87,31 +92,31 @@ class MovieTest extends GraphQLTestCase
 
     /**
      * @group  movie
+     * @group  testSaveWatchProgressMutation
      */
     public function testSaveWatchProgressMutation()
     {
         $query = file_get_contents(__DIR__ . '/movie/saveWatchProgressMutation.gql');
 
         $userHeaders = $this->getRandomUserHeaders(User::first());
-        $movie_id    = Movie::first()->id;
-        $variables   = [
-            'movie_id'     => $movie_id,
+        $movie_id = Movie::first()->id;
+        $variables = [
+            'movie_id' => $movie_id,
             'series_index' => 0,
-            'progress'     => 100,
+            'progress' => "100",
         ];
         $this->startGraphQL($query, $variables, $userHeaders);
     }
 
     /**
      * @group  movie
+     * @group  testShowMovieHistoryQuery
      */
     public function testShowMovieHistoryQuery()
     {
-        $query       = file_get_contents(__DIR__ . '/movie/showMovieHistoryQuery.gql');
+        $query = file_get_contents(__DIR__ . '/movie/showMovieHistoryQuery.gql');
         $userHeaders = $this->getRandomUserHeaders(User::first());
-        $movie_id    = Movie::first()->id;
-        $variables   = [
-            'movie_id' => $movie_id,
+        $variables = [
         ];
         $this->startGraphQL($query, $variables, $userHeaders);
     }
