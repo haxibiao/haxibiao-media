37602a4212221e5b227879be7c9fde60dda1c6b7
龚国玮 <gongguowei@haxifang.com>
2021-01-26 22:24:44 +0800

    test: ImageTest 和 SpiderTest 补全

--- tests/Feature/GraphQL/ImageTest.php
+++ tests/Feature/GraphQL/ImageTest.php
@@ -7,13 +7,45 @@ use Haxibiao\Breeze\GraphQLTestCase;
 class ImageTest extends GraphQLTestCase
 {
 
-    public function testUploadImageMutation()
+    /* --------------------------------------------------------------------- */
+    /* ------------------------------- Query ------------------------------- */
+    /* --------------------------------------------------------------------- */
+
+    /**
+     * 图片查询
+     *
+     * @group  image
+     */
+    public function testImageQuery()
     {
-        $token   = $this->getRandomUser()->api_token;
-        $query   = file_get_contents(__DIR__ . '/image/UploadImageMutation.gql');
+        $token = $this->getRandomUser()->api_token;
+        $query = file_get_contents(__DIR__ . '/image/ImageQuery.gql');
         $headers = [
             'Authorization' => 'Bearer ' . $token,
-            'Accept'        => 'application/json',
+            'Accept' => 'application/json',
+        ];
+        $variables = [
+            'id' => 1,
+        ];
+        $this->runGQL($query, $variables, $headers);
+    }
+
+    /* --------------------------------------------------------------------- */
+    /* ------------------------------- Mutation ---------------------------- */
+    /* --------------------------------------------------------------------- */
+
+    /**
+     * 上传图片
+     *
+     * @group  image
+     */
+    public function testUploadImageMutation()
+    {
+        $token = $this->getRandomUser()->api_token;
+        $query = file_get_contents(__DIR__ . '/image/UploadImageMutation.gql');
+        $headers = [
+            'Authorization' => 'Bearer ' . $token,
+            'Accept' => 'application/json',
         ];
         $variables = [
             'image' => [
--- tests/Feature/GraphQL/SpiderTest.php
+++ tests/Feature/GraphQL/SpiderTest.php
@@ -1,8 +1,9 @@
 <?php
 namespace Tests\Feature\GraphQL;
 
-use App\Spider;
+
 use App\User;
+use Haxibiao\Media\Spider;
 use Haxibiao\Breeze\GraphQLTestCase;
 
 class SpiderTest extends GraphQLTestCase
@@ -12,27 +13,42 @@ class SpiderTest extends GraphQLTestCase
     protected function setUp(): void
     {
         parent::setUp();
-        $this->user = User::factory()->create([
+        $this->user = User::factory()->make([
             'api_token' => str_random(60),
             'ticket'    => 100,
             'account'   => rand(10000000000, 99999999999),
         ]);
     }
+
+    /* --------------------------------------------------------------------- */
+    /* ------------------------------- Mutation ---------------------------- */
+    /* --------------------------------------------------------------------- */
+    
     /**
+     * 采集抖音视频里的分享地址
+     * 
      * @group spider
      */
-    public function testResolveDouyinVideo()
-    {
-        //确保后面UT不重复
-        $rows      = Spider::where('source_url', 'https://v.douyin.com/vruTta/')->delete();
-        $query     = file_get_contents(__DIR__ . '/spider/ResolveDouyinVideo.gql');
-        $variables = [
-            'share_link' => "#在抖音，记录美好生活#美元如何全球褥羊毛？经济危机下，2万亿救市的深层动力，你怎么看？#经济 #教育#云上大课堂 #抖音小助手 https://v.douyin.com/vruTta/ 复制此链接，打开【抖音短视频】，直接观看视频！",
-        ];
-        $user = User::where("ticket", ">", 10)->first();
-        $this->runGuestGQL($query, $variables, $this->getOneHeaders($user));
-    }
+    // public function testResolveDouyinVideoMutation()
+    // {
+    //     //确保后面UT不重复
+    //     $rows      = Spider::where('source_url', 'https://v.douyin.com/vruTta/')->delete();
+    //     $query     = file_get_contents(__DIR__ . '/spider/ResolveDouyinVideo.gql');
+    //     $variables = [
+    //         'share_link' => "#在抖音，记录美好生活#美元如何全球褥羊毛？经济危机下，2万亿救市的深层动力，你怎么看？#经济 #教育#云上大课堂 #抖音小助手 https://v.douyin.com/vruTta/ 复制此链接，打开【抖音短视频】，直接观看视频！",
+    //     ];
+    //     $this->runGuestGQL($query, $variables, $this->getOneHeaders($this->user));
+    // }
 
+    /* --------------------------------------------------------------------- */
+    /* ------------------------------- Query ------------------------------- */
+    /* --------------------------------------------------------------------- */
+
+    /**
+     * 采集抖音视频的爬虫
+     * 
+     * @group spider
+     */
     public function testSpidersQuery()
     {
         $query     = file_get_contents(__DIR__ . '/spider/SpidersQuery.gql');
@@ -46,12 +62,8 @@ class SpiderTest extends GraphQLTestCase
 
     public function getOneHeaders($user)
     {
-        $token = $user->api_token;
-
         $headers = [
-            'token'         => $token,
-            'Authorization' => 'Bearer ' . $token,
-            'Accept'        => 'application/json',
+            'Authorization' => 'Bearer ' . 'BvsQolpkVH3EUMvjF4NOUTtbYcrbzuG196tVaN46qgUVV80nq1SgOoJkWxof',
         ];
 
         return $headers;
