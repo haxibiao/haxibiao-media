e11de45830fb33166833792b466e20334fa0b11f
ivan <ivan@mac>
2021-03-28 17:37:37 +0800

    修复 seires_name 拼写错误，完善movie_histories兼容升级旧网站movie

--- database/migrations/2021_03_02_105630_add_columns_to_movie_histories.php
+++ database/migrations/2021_03_02_105630_add_columns_to_movie_histories.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddSeriesNameOnMovieHistoriesTable extends Migration
+class AddColumnsToMovieHistories extends Migration
 {
     /**
      * Run the migrations.
@@ -14,9 +14,14 @@ class AddSeriesNameOnMovieHistoriesTable extends Migration
     public function up()
     {
         Schema::table('movie_histories', function (Blueprint $table) {
-            if (!Schema::hasColumn('movie_histories', 'seires_name')) {
+            if (!Schema::hasColumn('movie_histories', 'series_name')) {
                 $table->string('series_name')->nullable();
             }
+
+            // 修复旧表字段结构
+            $table->string('progress')->nullable()->change();
+            $table->string('last_watch_time')->nullable()->change();
+            $table->unsignedInteger('series_id')->nullable()->change();
         });
     }
 
