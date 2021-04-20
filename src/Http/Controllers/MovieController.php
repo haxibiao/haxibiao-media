12d1ceb30389b0cf1d1e4536bb18f9c9564d5f52
ivan <ivan@haxibiao.com>
2021-04-20 10:29:48 +0800

    电影详情页性能优化 - sql

--- src/Http/Controllers/MovieController.php
+++ src/Http/Controllers/MovieController.php
@@ -8,7 +8,6 @@ use App\Movie;
 use App\MovieHistory;
 use App\User;
 use Haxibiao\Media\Http\Controller;
-use Haxibiao\Media\Scopes\MovieStatusScope;
 use Haxibiao\Media\SearchLog;
 use Illuminate\Http\Request;
 
@@ -228,8 +227,16 @@ class MovieController extends Controller
         $more = $qb->take(6)->get();
         //FIXME: 用sns 实现是否已收藏...
         $movie->favorited = false;
-        $recommend        = Movie::enable()->withoutGlobalScopes([new MovieStatusScope])->where('cover', 'like', '%cdn%')->latest('rank')->inRandomOrder()->take(6)->get();
-        $more             = Movie::enable()->withoutGlobalScopes([new MovieStatusScope])->where('cover', 'like', '%cdn%')->latest('rank')->inRandomOrder()->take(6)->get();
+
+        // 推荐同导演（性能考虑）
+        $qb          = Movie::latest('id');
+        $qb_producer = $qb->where('producer', $movie->producer)->where('id', '<>', $movie->id);
+        $recommend   = $qb_producer->where('cover', 'like', 'https://cdn-%')->latest('rank')->inRandomOrder()->take(6)->get();
+
+        //更多同地区
+        $qb         = Movie::latest('id');
+        $qb_country = $qb->where('country', $movie->country)->where('type', $movie->type)->where('id', '<>', $movie->id);
+        $more       = $qb_country->where('cover', 'like', 'https://cdn-%')->latest('rank')->inRandomOrder()->take(6)->get();
 
         // 兼容内涵电影vue用的series属性
         $movie->series = $movie->data;
