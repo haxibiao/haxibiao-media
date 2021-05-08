cf79539b29785b076f512078af858a939cd855e3
wuxiuzhang <wuxiuzhang@haxibiao.com>
2021-05-08 16:47:51 +0800

    fix：电影历史记录获取不到

--- src/Traits/MovieHistoryResolvers.php
+++ src/Traits/MovieHistoryResolvers.php
@@ -2,7 +2,6 @@
 
 namespace Haxibiao\Media\Traits;
 
-use App\MovieHistory as AppMovieHistory;
 use Haxibiao\Media\MovieHistory;
 use Illuminate\Support\Facades\DB;
 
@@ -11,20 +10,20 @@ trait MovieHistoryResolvers
     //resolvers
     public static function saveWatchProgress($root, $args, $content, $info)
     {
-        $movie_id = data_get($args, 'movie_id');
+        $movie_id     = data_get($args, 'movie_id');
         $series_index = data_get($args, 'series_index');
-        $progress = data_get($args, 'progress');
-        app_track_event('长视频','保存观看记录','电影id:'.$movie_id);
+        $progress     = data_get($args, 'progress');
+        app_track_event('长视频', '保存观看记录', '电影id:' . $movie_id);
         if (checkUser()) {
             $user = getUser();
             // 保存观看历史,存储每集的进度
             //$series_id,非series表主键，存储的值为series数组索引index
             MovieHistory::updateOrCreate([
-                'user_id' => $user->id,
-                'movie_id' => $movie_id,
+                'user_id'   => $user->id,
+                'movie_id'  => $movie_id,
                 'series_id' => $series_index,
             ], [
-                'progress' => $progress,
+                'progress'        => $progress,
                 'last_watch_time' => now(),
             ]);
         }
@@ -33,19 +32,22 @@ trait MovieHistoryResolvers
 
     public static function showMovieHistoryResolver($root, $args, $content, $info)
     {
-        app_track_event('长视频','查看长视频历史记录');
-        //取每个电影的最新一条剧集记录 
+        app_track_event('长视频', '查看长视频历史记录');
+        //标记获取详情数据信息模式
+        request()->request->add(['fetch_sns_detail' => true]);
+
+        //取每个电影的最新一条剧集记录
         if (checkUser()) {
             $user = getUser();
 
-            $var= MovieHistory::whereIn(DB::raw('(movie_id,updated_at)'),function ($query)use($user) {
-                    $query->select('movie_id',DB::raw('max(updated_at)'))
-                        ->from('movie_histories')
-                        ->where('user_id', $user->id)
-                        ->groupBy('movie_id');
-                }
+            $var = MovieHistory::whereIn(DB::raw('(movie_id,updated_at)'), function ($query) use ($user) {
+                $query->select('movie_id', DB::raw('max(updated_at)'))
+                    ->from('movie_histories')
+                    ->where('user_id', $user->id)
+                    ->groupBy('movie_id');
+            }
             )->orderByDesc('updated_at');
-            return ( $var);
+            return ($var);
         }
         return [];
     }
