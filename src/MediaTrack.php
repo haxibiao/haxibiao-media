36ed813470dc4aef73ad8299bd98cfe14e018f56
xxm <xiaoxinming@haxibiao.com>
2021-07-26 18:04:59 +0800

    GC-340 feat:增加媒体跟踪时长的二次累加

--- src/MediaTrack.php
+++ src/MediaTrack.php
@@ -5,6 +5,7 @@ namespace Haxibiao\Media;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\Relation;
+use Illuminate\Support\Arr;
 use Illuminate\Support\Carbon;
 
 class MediaTrack extends Model
@@ -12,6 +13,17 @@ class MediaTrack extends Model
     use HasFactory;
 
     protected $guarded = [];
+
+    protected $casts = [
+        'start_time' => 'datetime',
+        'end_time'   => 'datetime',
+    ];
+
+    public function addTrackSeconds($addedSeconds)
+    {
+        $this->track_seconds += $addedSeconds;
+        $this->end_time = now();
+    }
 
     public static function recrod($model, $startTime, $endTime, $userId = 0, $uuid = '')
     {
@@ -31,11 +43,23 @@ class MediaTrack extends Model
 
     public static function resolveMediaTrackReport($root, $args, $context, $info)
     {
-        $modelClass = Relation::getMorphedModel($args['media_type']);
-        $model      = $modelClass::find($args['media_id']);
-        $userId     = data_get(getUser(false), 'id', 0);
-        if ($model) {
-            return MediaTrack::recrod($model, $args['start_time'], $args['end_time'], $userId);
+        $trackId = Arr::get($args, 'track_id');
+        $track   = !empty($trackId) ? MediaTrack::find($args['track_id']) : null;
+
+        if (is_null($track)) {
+            $modelClass = Relation::getMorphedModel($args['media_type']);
+            $model      = $modelClass::find($args['media_id']);
+            $userId     = data_get(getUser(false), 'id', 0);
+            if ($model) {
+                $track = MediaTrack::recrod($model, $args['start_time'], $args['end_time'], $userId);
+            }
+        } else {
+            if ($args['end_time'] > $args['start_time'] && $args['end_time'] > $track->end_time) {
+                $track->addTrackSeconds($args['end_time']->diffInSeconds($args['start_time']));
+                $track->save();
+            }
         }
+
+        return $track;
     }
 }
