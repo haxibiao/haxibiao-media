b14211b0a05cf665d81f45be4f7515ac046ca539
杨倩 <yangqian@haxifang.cn>
2021-08-18 16:33:57 +0800

    调试cos上传图片

--- src/Traits/ActivityRepo.php
+++ src/Traits/ActivityRepo.php
@@ -31,6 +31,7 @@ trait ActivityRepo
         $file->storeAs($folder, $filename);
         $cloud_path = sprintf("%s/%s", $folder, $filename);
         $cdnurl     = cdnurl($cloud_path);
+        info('nova 上传图片。。' . $cdnurl);
         return $cdnurl;
     }
 }
