96bb01bda13101e09e6502c1767b49326ae41ee2
曾达威 <zengdawei@haxibiao.com>
2021-08-25 09:30:27 +0800

    feat: 增加 searchable:movie 命令用于手动填充影片数据到 meilisearcha

---
+++ src/Console/SearchableMovie.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Haxibiao\Media\Console;
+
+use App\Movie;
+use Illuminate\Console\Command;
+use MeiliSearch\Client;
+
+class SearchableMovie extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'searchable:movie';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = '手动将影片数据填充到 MeiliSearch 库';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $qb        = Movie::query()->whereNotNull('name');
+        $masterKey = env('MEILISEARCH_KEY');
+        $host      = env('MEILISEARCH_HOST');
+        if (empty($masterKey)) {
+            $this->error("请先在 .env 中补充 'MEILISEARCH_KEY' ");
+            return;
+        }
+        if (empty($host)) {
+            $this->error("请先在 .env 中补充 'MEILISEARCH_HOST' ");
+            return;
+        }
+        $client = new Client($host, $masterKey);
+        $index  = $client->index(config('app.name'));
+        $qb->chunkById(1000, function ($movies) use (&$index) {
+            $documents = [];
+            foreach ($movies as $movie) {
+                $documents[] = [
+                    'name' => $movie->name,
+                    'id'   => $movie->id,
+                ];
+            }
+            $result   = $index->addDocuments($documents);
+            $updateID = $result['updateId'];
+            $this->info("update ID: $updateID");
+        });
+        return 0;
+    }
+}
