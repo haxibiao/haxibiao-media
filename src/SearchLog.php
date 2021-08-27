b7a036d79a2a3ee69dc10cd032c29d17342e5b6f
杨倩 <yangqian@haxifang.cn>
2021-08-27 16:07:03 +0800

    JUHAOKAN-43 完善"大家都在搜"接口

--- src/SearchLog.php
+++ src/SearchLog.php
@@ -4,8 +4,10 @@ namespace Haxibiao\Media;
 
 use Haxibiao\Breeze\Model;
 use Haxibiao\Breeze\Traits\HasFactory;
+use GraphQL\Type\Definition\ResolveInfo;
 use Haxibiao\Media\Traits\SearchLogRepo;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Nuwave\Lighthouse\Support\Contracts\GraphQLContext;
 
 class SearchLog extends Model
 {
@@ -26,5 +28,26 @@ class SearchLog extends Model
             "questions"  => "题目",
             "categories" => "题库|分类",
         ];
+    }
+
+    public function resolveSearchLogs($root, array $args, GraphQLContext $context, ResolveInfo $resolveInfo)
+    {
+        // 对搜索词排序 + 只允许在7日内
+        $query = SearchLog::orderBy('count','desc')->whereDate('updated_at','>',today()->subDay(7))->pluck('keyword')->toArray();
+        $resulets = array_unique($query);
+
+        // 判断该搜索词是否能对应影片
+        $logs = [];
+        foreach ($resulets as $resulet) {
+            $movie = Movie::where('name',$resulet)->first();
+            if(!$movie){
+                continue;
+            }
+            $logs[] = $movie->name;
+        }
+        if(count($logs) > 6){
+            return array_slice($logs,0,6);
+        }  
+        return $logs;
     }
 }
