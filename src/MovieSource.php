0fa0e58cfcc64bc1dc5630b4b1f108a83948015f
wuxiuzhang <wuxiuzhang@haxibiao.com>
2021-09-30 20:03:51 +0800

    兼容本地movie_sources

--- src/MovieSource.php
+++ src/MovieSource.php
@@ -2,22 +2,31 @@
 
 namespace Haxibiao\Media;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class MovieSource extends Model
 {
     use HasFactory;
 
     protected $guarded = [];
-    
-    protected $casts   = [
+
+    protected $casts = [
         'play_urls' => 'array',
     ];
 
     public function movie(): BelongsTo
     {
         return $this->belongsTo(Movie::class);
+    }
+
+    //兼容本地movies，和共享的medichain模式
+    public function getTable()
+    {
+        if (config('media.enable_mediachain')) {
+            return "mediachain.movie_sources";
+        }
+        return 'movies';
     }
 }
