803f7202c8a5bd48ad67e9570e36660b58aa7ec0
杨倩 <yangqian@haxifang.cn>
2021-10-15 16:00:48 +0800

    同步影片时，处理“是否具备播放线路”的数据

--- src/Observers/MovieObserver.php
+++ src/Observers/MovieObserver.php
@@ -20,10 +20,19 @@ class MovieObserver
         Movie::addMeiliSearch($movie);
     }
 
-    // public function saved(Movie $movie)
-    // {
-    //     Movie::addMeiliSearchIndex($movie);
-    // }
+    public function saving(Movie $movie)
+    {
+        $playLines = $movie->play_lines;        
+        if(count($playLines) > 0){
+            $has_playurl = 1;
+        }else{
+            $has_playurl = 0;
+        }
+
+        $movie->update([
+            'has_playurl' => $has_playurl,
+        ]);
+    }
 
     public function updating(Movie $movie)
     {
