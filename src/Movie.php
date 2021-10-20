f8157ee5223304b114f217e12c655d9103c3c6df
杨倩 <yangqian@haxifang.cn>
2021-10-20 14:08:32 +0800

    JUHAOKANTV-47 点赞(undefined Movie::resoureTypeCN())

--- src/Movie.php
+++ src/Movie.php
@@ -202,4 +202,18 @@ class Movie extends Model
             ->withPivot(['report_status']);
     }
 
+    public function resoureTypeCN()
+    {
+        $type = get_class($this);
+        if (str_contains($type, 'Post')) {
+            return "动态";
+        }
+        if (str_contains($type, 'Issue')) {
+            return "提问";
+        }
+        if (str_contains($type, 'Movie')) {
+            return "电影";
+        }
+        return '文章';
+    }
 }
